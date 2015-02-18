cccfa527a4a6aba254670d82e59bb580ea7cdc7b
Osa Gaius <osayame@gmail.com>
2015-02-18 16:39:11 -0500

    modified facebook login and page routing

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,18 @@ class User < ActiveRecord::Base
   	end
   end
 
+  def self.from_omniauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.firstname = auth.info.first_name
+      user.lastname = auth.info.last_name
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save!
+    end
+  end
+
   def login=(login)
     @login = login
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
 
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
