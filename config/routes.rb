a52be4f70863b1cd8d1ef95854c4bbf2d41cd500
Osayame Gaius <osayame@gmail.com>
2015-02-24 13:33:18 -0500

    Added question controller, views, and model

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   resources :games
+  
+  resources :questions
+
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
