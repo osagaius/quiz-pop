dfc70a8c555aa7147775027cb0b0d6cc67623e21
John Moore <me@johndm.com>
2015-03-10 10:01:18 -0400

    Added category menu and fixed asset compiling issues.

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -7,11 +7,32 @@ app.dashboard = new function(){
 
 
     this.init = function(){
+        this.spinIcons();
+        this.bindDropdown();
+    }
+
+    this.spinIcons = function(){
         $('.'+this.classes.navigation+" li").hover(function(){
             $(this).find('i').addClass('fa-spin');
         }, function(){
             $(this).find('i').removeClass('fa-spin');
         });
-    }
+    };
+
+    this.bindDropdown = function(){
+        var button = document.getElementById('submit-question');
+        var categories = document.getElementById('categories-dropdown');
+        var shown = false;
+
+        button.addEventListener('click', function(e){
+            if(shown){
+                categories.style.display = 'none';
+            }else{
+                categories.style.display = 'inline-block';
+            }
+
+            shown = !shown;
+        });
+    };
 
 };
