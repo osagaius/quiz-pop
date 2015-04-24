45234014652258e204cefa4d06906b5e9a12ba8a
Osayame Gaius-Obaseki <osayame@gmail.com>
2015-04-23 23:52:09 -0400

    modified new wheel to use spinner partial

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,4 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_directory .
+//= require_tree ./public
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.assets.precompile += %w( spinner.js )
+  config.assets.precompile += %w( mechanics.js )
 
 end
