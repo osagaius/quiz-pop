5f2efdafb10631a3fa2298615cd063778c6d0a94
Osayame Gaius-Obaseki <osayame@gmail.com>
2015-06-03 13:30:04 -0400

    Fixed signupp route error by adding controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admins
-  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/auth" }
+
   get 'welcome/index'
 
   resources :games do 
