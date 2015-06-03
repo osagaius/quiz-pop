7131773193c991a7e579bc2158d1f5f6f57499d0
Osayame Gaius-Obaseki <osayame@gmail.com>
2015-06-02 22:39:58 -0400

    Fixed redirection when wrong answer selected during special mode

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -25,20 +25,25 @@ class GamesController < ApplicationController
 
 	def show
 		@game = Game.find(params[:id])
-		@question = Question.first
-		@@question = @question
-		
-		if @game.streak
-			@game.special_mode = true
-			@game.save
-		end
-
-		if player1?(@game)
-			@current_user_pieces = @game.player_1_pieces
-			@opponent_pieces = @game.player_2_pieces
+		if current_user.id != @game.turn
+			redirect_to welcome_index_path
+			return
 		else
-			@current_user_pieces = @game.player_2_pieces
-			@opponent_pieces = @game.player_1_pieces
+			@question = Question.first
+			@@question = @question
+
+			if @game.streak
+				@game.special_mode = true
+				@game.save
+			end
+
+			if player1?(@game)
+				@current_user_pieces = @game.player_1_pieces
+				@opponent_pieces = @game.player_2_pieces
+			else
+				@current_user_pieces = @game.player_2_pieces
+				@opponent_pieces = @game.player_1_pieces
+			end
 		end
 	end
 
@@ -77,147 +82,155 @@ class GamesController < ApplicationController
 		@game = Game.find(params[:game_id])
 		@question = Question.find(params[:question_id])
 
-		# @question.rating = params[:rating]
+			# @question.rating = params[:rating]
 
-		@path = '/games/' + @game.id.to_s
-		@correct = params[:correct]
-		
-		puts "current special_mode"
-		puts @game.special_mode
+			@path = '/games/' + @game.id.to_s
+			@correct = params[:correct]
+			
+			puts "current special_mode"
+			puts @game.special_mode
 
-		if @game.special_mode
-			puts "entering special mode"
-			@game.special_mode = false
-			@game.streak = false
+			if @game.special_mode
+				puts "entering special mode"
+				@game.special_mode = false
+				@game.streak = false
+				@game.save
+				if @correct === 'true'
+					redirect_to_game(@game)
+					return
+				else
+					redirect_to_home
+				end
+			else
+				if @correct === 'true'
+					if @game.streak
+						render action: 'special.js.erb'
+					else
+						redirect_to_game(@game)
+					end
+				else			
+					respond_to do |format|
+						format.js
+					end
+				end
+			end
+		end
+
+		def specialMode
+			@game = Game.find(params[:game_id])
+			if params[:choice] == 'piece'
+				@game.challenge = false
+			else
+				@game.challenge = true
+			end
 			@game.save
 			redirect_to_game(@game)
-			return
-		else
-			if @correct === 'true'
-				if @game.streak
-					render action: 'special.js.erb'
-				else
-					redirect_to_game(@game)
-				end
-			else			
+		end
+
+		def updateQuestion
+			@category = params[:category]
+			@game = Game.find(params[:id])
+			@game.current_category = params[:category]
+			if @game.save
 				respond_to do |format|
-					format.js
+					format.js { render :partial => "question" }
 				end
+			else
+				render json: { message: "Error changing category", status: :not_found }
 			end
 		end
-	end
 
-	def specialMode
-		@game = Game.find(params[:game_id])
-		if params[:choice] == 'piece'
-			@game.challenge = false
-		else
-			@game.challenge = true
+		def edit
+			@game = Game.find(params[:id])
 		end
-		@game.save
-		redirect_to_game(@game)
-	end
 
-	def updateQuestion
-		@category = params[:category]
-		@game = Game.find(params[:id])
-		@game.current_category = params[:category]
-		if @game.save
-			respond_to do |format|
-				format.js { render :partial => "question" }
+		def update
+			@game = Game.find(params[:id])
+
+			if @game.update_attributes(game_params)
+				redirect_to @game
 			end
-		else
-			render json: { message: "Error changing category", status: :not_found }
+		end
+
+		def new
+			@game = Game.new
+			WillPaginate.per_page = 5
+			@users = User.paginate(page: params[:page])
+		end
+
+		def destroy
+			@game = Game.find(params[:id])
+			@game.complete = true
+			if (current_user.id == @game.player1)
+				@game.winner = @game.player2
+				@game.loser = current_user.id
+				@game.player_2_score = @game.player_2_score + 1
+
+				@game.winner = @game.player1
+				@game.loser = current_user.id
+				@game.player_1_score+=1
+			end
+			@game.save
+			redirect_to welcome_index_path
+		end
+
+		private
+
+		def increment_meter(a_game)
+			@game = Game.find(a_game.id)
+			@game.meter += 1
+			if @game.meter >= 3
+				@game.streak = true
+			else
+				@game.streak = false
+			end
+
+			@game.save
+		end
+		def reset_meter(a_game)
+			@game = Game.find(a_game.id)
+			@game.meter = 0
+			@game.save
+		end
+		def change_turn(a_game)
+			@game = Game.find(a_game.id)
+			if @game.player1 == current_user.id
+				@game.turn = @game.player2
+			else
+				@game.turn = @game.player1
+			end
+			@game.save
+		end
+
+		def redirect_to_game(game)
+			@game = Game.find(game.id)
+			@path = '/games/' + @game.id.to_s
+			render js: %(window.location.pathname='#{@path}')
+		end
+
+		def add_player_piece(game)
+			@game = Game.find(game.id)
+			@piece = Category.find(game.current_category).title
+			if player1?(game)
+				@game.player_1_pieces += [@piece]
+			else
+				@game.player_2_pieces += [@piece]
+			end
+			@game.save
+		end
+
+		def player1?(game)
+			game.player1 == current_user.id
+		end
+
+		def player2(game)
+			game.player1 == current_user.id
+		end
+		def redirect_to_home
+			@path = welcome_index_path
+			render js: %(window.location.pathname='#{@path}')
+		end
+		def game_params
+			params.require(:game).permit(:player1, :player2, :question)
 		end
 	end
-
-	def edit
-		@game = Game.find(params[:id])
-	end
-
-	def update
-		@game = Game.find(params[:id])
-		
-		if @game.update_attributes(game_params)
-			redirect_to @game
-		end
-	end
-
-	def new
-		@game = Game.new
-		WillPaginate.per_page = 5
-		@users = User.paginate(page: params[:page])
-	end
-
-	def destroy
-		@game = Game.find(params[:id])
-		@game.complete = true
-		if (current_user.id == @game.player1)
-			@game.winner = @game.player2
-			@game.loser = current_user.id
-			@game.player_2_score = @game.player_2_score + 1
-
-			@game.winner = @game.player1
-			@game.loser = current_user.id
-			@game.player_1_score+=1
-		end
-		@game.save
-		redirect_to welcome_index_path
-	end
-
-	private
-
-	def increment_meter(a_game)
-		@game = Game.find(a_game.id)
-		@game.meter += 1
-		if @game.meter >= 3
-			@game.streak = true
-		else
-			@game.streak = false
-		end
-		
-		@game.save
-	end
-	def reset_meter(a_game)
-		@game = Game.find(a_game.id)
-		@game.meter = 0
-		@game.save
-	end
-	def change_turn(a_game)
-		@game = Game.find(a_game.id)
-		if @game.player1 == current_user.id
-			@game.turn = @game.player2
-		else
-			@game.turn = @game.player1
-		end
-		game.save
-	end
-
-	def redirect_to_game(game)
-		@game = Game.find(game.id)
-		@path = '/games/' + @game.id.to_s
-		render js: %(window.location.pathname='#{@path}')
-	end
-
-	def add_player_piece(game)
-		@game = Game.find(game.id)
-		@piece = Category.find(game.current_category).title
-		if player1?(game)
-			@game.player_1_pieces += [@piece]
-		else
-			@game.player_2_pieces += [@piece]
-		end
-		@game.save
-	end
-
-	def player1?(game)
-		game.player1 == current_user.id
-	end
-
-	def player2(game)
-		game.player1 == current_user.id
-	end
-	def game_params
-		params.require(:game).permit(:player1, :player2, :question)
-	end
-end
