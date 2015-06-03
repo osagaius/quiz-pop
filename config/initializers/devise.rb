1d8a2c1bdafaaf841b1c3aa776ac9b3f98186c82
Osayame Gaius-Obaseki <osayame@gmail.com>
2015-06-03 13:13:06 -0400

    Fixed config.sign_out_via to use get instead of delete

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -235,7 +235,7 @@ config.stretches = Rails.env.test? ? 1 : 10
   # config.navigational_formats = ['*/*', :html]
 
   # The default HTTP method used to sign out a resource. Default is :delete.
-  config.sign_out_via = :delete
+  config.sign_out_via = :get
 
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :admins
-  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
   get 'welcome/index'
 
   resources :games do 
