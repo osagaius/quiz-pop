4d2c71d959d3ee716ed0a41bb4acebe440700f20
Osayame Gaius-Obaseki <osayame@gmail.com>
2015-07-20 18:43:21 -0400

    completed user personal stats for categories

--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -4,7 +4,7 @@ class Statistic < ActiveRecord::Base
 	def self.insert(user, game, question, correct)
 		@stat = Statistic.new
 		@stat.user_id = user.id
-		@stat.category = game.current_category
+		@stat.category = Category.find(game.current_category).title
 		@stat.correct = correct
 		@stat.question = question.id
 		@stat.game_id = game.id
@@ -24,13 +24,25 @@ class Statistic < ActiveRecord::Base
 		stats_by_period("month")
 	end
 
+	def self.user_stats(user, category)
+		@correct = Statistic.where(["user_id = ? and correct = ? and category = ?", user.id, true, category]).count
+
+		if @correct != 0 
+			@accuracy = (@correct/Statistic.where({user_id: user.id, category: category}).count) * 100
+		else
+			@accuracy = 0
+		end
+		puts '!!!!!!!!!!!!' + @accuracy.to_s
+		@accuracy
+	end
+
 	def self.stats_by_period(period)
 		time = {"week" => 1.week.ago.utc, "month" => 1.month.ago.utc, "none" => 5000.month.ago.utc}
 		@stats = Hash.new
 		User.all.each do |user|
 			id = user.id
 			@correct = Statistic.where(["user_id = ? and correct = ? and updated_at >= ?", user.id, true, time[period]]).count
-			
+
 			if @correct != 0 
 				@accuracy = (@correct/Statistic.where({user_id: user.id}).count) * 100
 			else
@@ -44,7 +56,7 @@ class Statistic < ActiveRecord::Base
 		@stats.sort_by { |k,v| v[:accuracy] }.reverse
 	end
 
-	
+
 
 
 end
